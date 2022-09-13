1f486a321f7910dfc709dea6f90d96f59408a6fe
cesarrua <cesarxrua@gmail.com>
2022-09-12 20:47:40 -0500

    actualizando

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ app.listen(process.env.PORT || 3000, () => {
     console.log("Servidor corriendo en el puerto 3000")
 });
 
+app.get(("/"), (req, res) =>{
+    res.redirect("/home")
+})
 
 app.get(("/home"), (req, res) =>{
     res.sendFile(path.join(__dirname, "/views/home.html"))
