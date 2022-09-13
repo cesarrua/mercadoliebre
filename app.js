97444d4e7fef3a5f2399b45b30c7cbdd113dd803
cesarrua <cesarxrua@gmail.com>
2022-09-12 20:30:07 -0500

    cambiando el procfile

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 app.use(express.static("public"));
 
-app.listen(procces.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Servidor corriendo en el puerto 3000")
 });
 
