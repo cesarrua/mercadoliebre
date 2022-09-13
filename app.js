b16e380a984ed72c252a79aaec2bab6b8ab30f5f
cesarrua <cesarxrua@gmail.com>
2022-09-12 20:22:22 -0500

    modificando el heroku

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const path = require("path");
 
 app.use(express.static("public"));
 
-app.listen(3000, () =>{
-    console.log("levantando el server en el puerto 3000")
-})
+app.listen(procces.env.PORT || 3000, () => {
+    console.log("Servidor corriendo en el puerto 3000")
+});
+
 
 app.get(("/home"), (req, res) =>{
     res.sendFile(path.join(__dirname, "/views/home.html"))
